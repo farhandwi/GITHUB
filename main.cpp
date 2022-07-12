08c1a1dbb10955dd6981ea2e8b6f59dd1d6144df
farhandwi <109074621+farhandwi@users.noreply.github.com>
2022-07-12 08:52:16 +0700

    menambah fungsi di main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
 using namespace std;
 
+int ADD(int a, int b){
+  cout << "Fungsi Tambah" << endl;
+  c = a+b;
+  cout << a << " + " << b << " = " << c << endl;
+  return c;
+}
+
 int main(){
   cout << "Hello World" << endl;
   int a, b;
