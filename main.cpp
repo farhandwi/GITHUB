093c92aebf9d2bdfba995682d53270f990c8b740
farhandwi <109074621+farhandwi@users.noreply.github.com>
2022-07-12 08:21:59 +0700

    menambah if else

--- main.cpp
+++ main.cpp
@@ -3,6 +3,14 @@ using namespace std;
 
 int main(){
   cout << "Hello World" << endl;
+  int a, b;
+  a = 10;
+  b = 11;
+  if(a > b){
+    cout << "Aku A" << endl;
+  }else{
+    cout << "Aku B" << endl;
+  }
   
   return 0;
 }
